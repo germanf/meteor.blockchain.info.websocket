94e541e3da3ec853ea1c2f6e491469a6837e4ead
Germán <ferreyra.ger@gmail.com>
2016-02-03 21:48:03 -0300

    Update package.js

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
-  name: 'mjr:blockchain-websockets',
-  version: '0.0.2',
+  name: 'germanf:blockchain-ws',
+  version: '0.0.5',
   // Brief, one-line summary of the package.
   summary: 'Wrapper for Blockchain.info websocket data stream',
   // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/joshuarossi/meteor.blockchain.info.websocket.git',
+  git: 'https://github.com/germanf/meteor.blockchain.info.websocket.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
